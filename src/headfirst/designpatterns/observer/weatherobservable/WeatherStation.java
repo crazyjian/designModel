055a1b2832cad2b31e295135545101de7e073ee0
Jerry <1076441760@qq.com>
2017-02-28 22:54:39 +0800

    气象站（观察者模式）

---
+++ src/headfirst/designpatterns/observer/weatherobservable/WeatherStation.java
@@ -0,0 +1,21 @@
+package headfirst.designpatterns.observer.weatherobservable;
+
+import headfirst.designpatterns.observer.weather.ForecastDisplay;
+import headfirst.designpatterns.observer.weather.StatisticsDisplay;
+import headfirst.designpatterns.observer.weather.CurrentConditionsDisplay;
+import headfirst.designpatterns.observer.weather.WeatherData;
+
+public class WeatherStation {
+
+	public static void main(String[] args) {
+		WeatherData weatherData = new WeatherData();
+		
+		CurrentConditionsDisplay currentConditions = new CurrentConditionsDisplay(weatherData);
+		StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+		ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+
+		weatherData.setMeasurements(80, 65, 30.4f);
+		weatherData.setMeasurements(82, 70, 29.2f);
+		weatherData.setMeasurements(78, 90, 29.2f);
+	}
+}
